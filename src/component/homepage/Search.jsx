cd227f41a7382e9037687e6a7c4eeefb3ae55ed6
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-02-29 06:37:32 -1200

    I just changed something on the search and navbar

--- src/component/homepage/Search.jsx
+++ src/component/homepage/Search.jsx
@@ -1,53 +1,55 @@
-
 'use client'
-import React,{ useState }  from 'react'
-import { Button, Input, Tooltip } from '@mui/material'
 
-
-
+import React, { useState } from 'react';
+import { Button, Tooltip } from '@mui/material';
 
 const Search = () => {
-  const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div style={{
-        width: "100%",
-        height: "40px",
-        border: "1px solid #353535",
-        padding: 0,
-        margin: 0,
-        display: "flex",
-        borderRadius: "5px",
-        fontWeight: "bold",
-        outline:'none',
+      display: "flex",
+      alignItems: "center",
+      width: "100%",
+      height: "40px",
+      border: "1px solid #353535",
+      borderRadius: "5px",
+      overflow: "hidden",
+      outline: "none",
     }}>
-        <Input  
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
+      <input
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        style={{
+          flex: 1,
+          border: "none",
+          padding: "0 12px",
+          height: "100%",
+          outline: "none",
+          textDecoration: "none",
+        }}
+        placeholder='Enter what you want to search here'
+      />
+      <Tooltip title='Search feature under construction' arrow>
+        <Button
           style={{
-            width: "100%",
+            width: "80px",
+            borderRadius: "0 4px 4px 0",
+            fontFamily: "inherit",
+            fontWeight: 300,
+            background: "#353535",
+            color: "white",
             border: "none",
-            borderRadius: "5px",
             outline: "none",
-            padding: "0 2px 0 2px",
-            textDecorationLine: "none",
-            
-          }} 
-          placeholder='Enter what you want search here' 
-            />
-        <Tooltip title='search feature under construction' arrow>
-          <Button style={{
-              width: "80px",
-              border: "none",
-              borderRadius: "5px",
-              fontFamily: "inherit",
-              fontWeight: 600,
-              background: "#353535",
-              color: "white",
-              outline:"none",
-          }}>Search</Button>
-        </Tooltip>
+            margin: 0,
+            height:'100%',
+          }}
+        >
+          Search
+        </Button>
+      </Tooltip>
     </div>
-  )
-}
+  );
+};
 
 export default Search;
