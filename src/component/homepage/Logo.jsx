706f74fd71cf34c5bf4ec94760a44037116a6194
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-02-29 01:37:20 -1200

    did some changes to the clicking of the logo

--- src/component/homepage/Logo.jsx
+++ src/component/homepage/Logo.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import {Box} from "@mui/material"
-
+'use client';
+import React from 'react';
+import { Box } from "@mui/material";
+import { useRouter } from 'next/navigation';
 
 const Logo = () => {
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.push('/');
+  };
+
   return (
     <div style={{
-        display :"flex",
-        justifyContent: "flex-start"
-}}>
-        <Box>
+      display: "flex",
+      justifyContent: "flex-start",
+      cursor: "pointer"  // Adding cursor pointer for better UX
+    }}>
+      <Box  onClick={handleLogoClick}>
         <img src="https://i.pinimg.com/564x/d8/23/e3/d823e34f9d58fef210dba91770dff573.jpg" alt="" width="50" height="50" />
-          </Box>
+      </Box>
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
