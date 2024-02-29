47158afa1e9eb1293550ce066740fac52230f433
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-02-29 02:01:28 -1200

    did some changes to the navbar

--- src/component/homepage/UserIcon.jsx
+++ src/component/homepage/UserIcon.jsx
@@ -1,33 +1,54 @@
-import React from 'react'
+
+'use client';
+import React from 'react';
 import { FaRegUserCircle } from "react-icons/fa";
-
-
+import { GrCart } from "react-icons/gr";
+import { Box } from "@mui/material";
 
 const UserIcon = () => {
-    return ( 
-        <div
-         style={{
-            display :"flex",
-            justifyContent: "flex-end"
-        }}
-        >
-            <div
-            style={{
-                height:'35px',
-                width:'35px',
-                backgroundColor:'',
-                alignItems:'center',
-                display:'flex',
-                justifyContent:'center',
-                borderRadius:'50%',
-                cursor:'pointer',
-            }}
-            >
-            <FaRegUserCircle  size={30} color='gray'/>
-            </div>
 
-  </div>
-     );
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        gap: '10px'
+      }}
+    >
+      <div
+        style={{
+          height: '35px',
+          width: '35px',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          borderRadius: '50%',
+          cursor: 'pointer', // Change background color based on click state
+        }}
+      >
+        <Box>
+        <GrCart size={23} color='black' />
+        </Box>
+       
+      </div>
+      <div
+        style={{
+          height: '35px',
+          width: '35px',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          borderRadius: '50%',
+          cursor: 'pointer', // Change background color based on click state
+        }}
+      >
+        <Box>
+        <FaRegUserCircle size={23} color='black' />
+        </Box>
+      </div>
+    </div>
+  );
 }
- 
+
 export default UserIcon;
