871547965e33ed0da5966315dcc93eeba78a7738
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-02 04:54:30 -1200

    started working on the category sections

--- src/components/homepage/master_home.jsx
+++ src/components/homepage/master_home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './navbar';
 import Banner from './banner';
+import CategorySection from './categories';
 
 const HomeMaster = () => {
     return ( 
         <div className=''>
             <Nav />
             <Banner />
+            <CategorySection/>
         </div>
      );
 }
---
+++ src/data/categories.js
@@ -0,0 +1,28 @@
+const categories=[
+    {
+        id:1,
+        group:'Kente Cloth',
+        imageUrl:''
+    },
+    {
+        id:2,
+        group:'Smock',
+        imageUrl:''
+    },{
+        id:3,
+        group:'Ankara',
+        imageUrl:'',
+    },{
+        id:4,
+        group:'Briskis',
+        imageUrl:''
+    },{
+        id:5,
+        group:'Adinkra',
+        imageUrl:''
+    },{
+        id:6,
+        group:'Kete',
+        imageUrl:'',
+    }
+]
