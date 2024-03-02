27db54f5f74e0da8e6d1a5d46b6df9183d593f90
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-02 08:14:11 -1200

    made some progress on the cloth looking site

--- src/components/homepage/categories.jsx
+++ src/components/homepage/categories.jsx
@@ -7,10 +7,10 @@ const CategorySection = () => {
             <div>
                 <h1 className='text-3xl font-mono font-semibold text-center'>Product Categories</h1>
             </div>
-            <div>
+            <div className='flex flex-row items-center justify-center flex-wrap w-full '>
                 {categories.map((category) => {
                     return (
-                        <div key={category.id}>
+                        <div key={category.id} className='flex-initial  m-2 p-4 border border-gray-300 w-36 h-36 bg-white shadow-md flex flex-col items-center justify-center'>
                             <img src={category.imageUrl} alt={category.group} />
                             <div>
                                 <h6>{category.group}</h6>
