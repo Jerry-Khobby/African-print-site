b8415dc2ed047de9b3baf01db319cd5fd10e2bd9
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-02 09:02:10 -1200

    done with the product sessions

--- src/components/homepage/categories.jsx
+++ src/components/homepage/categories.jsx
@@ -7,13 +7,13 @@ const CategorySection = () => {
             <div>
                 <h1 className='text-3xl font-mono font-semibold text-center'>Product Categories</h1>
             </div>
-            <div className='flex flex-row items-center justify-center flex-wrap w-full '>
+            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 justify-items-center'>
                 {categories.map((category) => {
                     return (
-                        <div key={category.id} className='flex-initial  m-2 p-4 border border-gray-300 w-40 h-40 bg-white shadow-md flex flex-col items-center justify-center hover:bg-black'>
-                            <img src={category.imageUrl} alt={category.group} className='w-20 h-20 rounded-full flex items-center justify-center '/>
-                            <div className='p-3'>
-                                <h6 className='font-semibold text-sm'>{category.group}</h6>
+                        <div className="border-2 border-gray-200 rounded-lg p-4 w-40 h-40 shadow-sm hover:bg-black hover:text-white" key={category.id}>
+                            <img src={category.imageUrl} alt={category.group} className='w-20 h-20 rounded-full flex items-center justify-center mx-auto'/>
+                            <div className='mt-4'>
+                                <h6 className='font-semibold text-sm text-center'>{category.group}</h6>
                             </div>
                         </div>
                     );
