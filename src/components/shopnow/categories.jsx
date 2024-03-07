7e576f474da7d59f0b90e33cd8476d1c4b91a7fb
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-07 04:24:18 -1200

    left with changing the images on the page

--- src/components/shopnow/categories.jsx
+++ src/components/shopnow/categories.jsx
@@ -17,7 +17,7 @@ const CategorySection = () => {
         <div>
         {Object.entries(groupedProducts).map(([groupName, products]) => (
         <div key={groupName}>
-          <h2 className='font-semibold text-lg font-serif text-start pt-6'>{groupName}</h2>
+          <h2 className='font-semibold text-2xl font-serif text-start pt-6'>{groupName}</h2>
           <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-items-center'>
             {products.map(product => (
               <div key={product.id} className='border-2 border-gray-200 rounded-lg p-4 w-48 h-52 shadow-sm hover:bg-black hover:text-white'>
