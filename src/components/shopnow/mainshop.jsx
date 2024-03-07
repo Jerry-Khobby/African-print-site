9d42239153ac15f150b9289e6605bdcf67226372
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-06 21:51:15 -1200

    I started work on the shop now page

---
+++ src/components/shopnow/mainshop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../homepage/navbar';
+
+
+
+const ShopNowMain = () => {
+    return ( 
+        <div>
+<Nav/>
+        </div>
+     );
+}
+ 
+export default ShopNowMain;
