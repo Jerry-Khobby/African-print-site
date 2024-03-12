1e7bcd5206c00b8be62a3bdc510c98caf89762e1
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-12 04:52:44 -1200

    I am done setting up my store and reducer

---
+++ src/lib/reducer.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState={
+    selectedProduct:null,
+};
+
+
+const productSlice=createSlice({
+    name:'product',
+    initialState,
+    reducers:{
+        selectedProduct:(state,action)=>{
+            state.selectedProduct = action.payload;
+        }
+    }
+});
+
+
+
+export const { selectProduct } = productSlice.actions;
+export const selectedProductSelector = (state) => state.product.selectedProduct;
+
+export default productSlice.reducer;
