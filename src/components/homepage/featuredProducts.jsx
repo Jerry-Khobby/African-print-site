cd1bb0a590d429a132f9172e40d3374986efa536
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-15 01:10:40 -1200

    adding the data to the store

--- src/components/homepage/featuredProducts.jsx
+++ src/components/homepage/featuredProducts.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { featuredProducts } from '@/data/categories';
 import Link from 'next/link';
+import {useDispatch} from "react-redux";
+import { selectProduct } from '@/lib/reducer';
 
 
 const FeaturedProducts = () => {
     // a function that will pick the random stuffs from the json file 
     // Function to get 8 random items from an array
+    const dispatch=useDispatch();
+
+    //Write a function that will handle the dispatch 
+    const handleProductClick = (randomItem) => {
+dispatch(selectProduct(randomItem));
+    }
 const getRandomItems = (array, count = 6) => {
     const shuffled = array.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
@@ -25,12 +33,12 @@ const getRandomItems = (array, count = 6) => {
 {
     randomItems.map((randomItem)=>{
         return(
-<div className="border-2 border-gray-200 rounded-lg p-4 w-48 h-48 shadow-sm hover:bg-black hover:text-white" key={randomItem.id}>
+<div className="border-2 border-gray-200 rounded-lg p-4 w-48 h-48 shadow-sm hover:bg-black hover:text-white" key={randomItem.id} onClick={handleProductClick}>
 <Link  href="/Products" style={{color:'inherit',textDecoration:'none'}}>
                  <img src={randomItem.imageUrl} alt={randomItem.group} className='w-20 h-20 rounded-full flex items-center justify-center mx-auto'/>
                  <div className='mt-0 pb-2'>
-                                <h6 className='font-medium text-xs text-center pt-1'>{randomItem.name}</h6>
-                                <h6 className='font-semibold text-xs text-center'>${randomItem.price}</h6>
+                <h6 className='font-medium text-xs text-center pt-1'>{randomItem.name}</h6>
+                <h6 className='font-semibold text-xs text-center'>${randomItem.price}</h6>
 </div>
 </Link>
             </div>
