f4fd868837de9788b64fa67500c5573d389b90a2
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-19 03:48:14 -1200

    done with the errors

--- src/components/products/eachproduct.jsx
+++ src/components/products/eachproduct.jsx
@@ -1,20 +1,20 @@
+// EachProduct.jsx
 'use client'
-import React from 'react'
+import React from 'react';
 import { useAppSelector } from '@/lib/hooks';
 
-
-
-
 const EachProduct = () => {
-    //initializing the redux to unleash the items to the new screen 
-    const nextItem=useAppSelector((state)=>state.product.selectedProduct)
-    return ( 
-    <div>
-    {nextItem.id}
-    {nextItem.description}
-    {nextItem.group}
-    </div>
-     );
-}
- 
+    // Initializing the redux to unleash the items to the new screen 
+    const nextItem = useAppSelector((state) => state.product.selectedProduct);
+
+    // Check if nextItem is not null before rendering its properties
+    return (
+        <div>
+            {nextItem && (
+    <></>
+            )}
+        </div>
+    );
+};
+
 export default EachProduct;
