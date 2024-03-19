19ea753dd66bcce48840b2fd44f529a197b5394c
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-19 03:24:20 -1200

    the issue with the local storage have been fixed

--- src/components/products/eachproduct.jsx
+++ src/components/products/eachproduct.jsx
@@ -9,8 +9,10 @@ const EachProduct = () => {
     //initializing the redux to unleash the items to the new screen 
     const nextItem=useAppSelector((state)=>state.product.selectedProduct)
     return ( 
-    <div className=''>
-    
+    <div>
+    {nextItem.id}
+    {nextItem.description}
+    {nextItem.group}
     </div>
      );
 }
--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -1,23 +1,30 @@
+// productSlice.js
+
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState={
-    selectedProduct: JSON.parse(localStorage.getItem("selectedProduct")) || null,
+const getInitialSelectedProduct = () => {
+    if (typeof localStorage !== "undefined" && localStorage.getItem("selectedProduct")) {
+        return JSON.parse(localStorage.getItem("selectedProduct"));
+    }
+    return null;
 };
 
+const initialState = {
+    selectedProduct: getInitialSelectedProduct(), // Call the function to get the initial value
+};
 
-export const productSlice=createSlice({
-    name:'product',
+export const productSlice = createSlice({
+    name: 'product',
     initialState,
-    reducers:{
-        selectedProduct:(state,action)=>{
+    reducers: {
+        selectedProduct: (state, action) => {
             state.selectedProduct = action.payload;
-            localStorage.setItem("selectedProduct", JSON.stringify(action.payload));
-        }
-    }
+            if (typeof localStorage !== "undefined") {
+                localStorage.setItem("selectedProduct", JSON.stringify(action.payload));
+            }
+        },
+    },
 });
-
-
 
 export const { selectedProduct } = productSlice.actions;
 export default productSlice.reducer;
