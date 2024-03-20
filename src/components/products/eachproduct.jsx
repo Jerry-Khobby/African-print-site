e48728cca13a440384e277ae2c78032ed2163d74
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-20 04:38:23 -1200

    done with that error

--- src/components/products/eachproduct.jsx
+++ src/components/products/eachproduct.jsx
@@ -8,11 +8,15 @@ const EachProduct = () => {
     // Initializing the redux to unleash the items to the new screen 
     const nextItem = useAppSelector((state) => state.product.selectedProduct);
     // Check if nextItem is not null before rendering its properties
+    if(!nextItem){
+        return null;
+    }
     return(
     <div className='w-full  flex  flex-row items-center sm:gap-10 md:gap-24 lg:gap-32 gap-10 justify-center mt-24  flex-wrap '>
 <div className='border-2 border-gray-200 rounded-lg p-4 w-56 h-56 shadow-sm  hover:text-white'>
-    {/** the div will have to have the image of the  */}
-    <img src={nextItem.imageUrl} alt="The image for the product" className="w-52 h-48 rounded-sm flex items-center justify-center self-center"/>
+    {
+        nextItem.imageUrl&&    <img src={nextItem.imageUrl} alt="The image for the product" className="w-52 h-48 rounded-sm flex items-center justify-center self-center"/>
+    }
 </div>
 <div>
     <div className='flex items-start flex-col justify-center'>
