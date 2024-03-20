617a9c55a18ca77aa757d1b0c455c45c64966ec2
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-20 03:10:50 -1200

    I have started fixing for each product page

--- src/components/products/eachproduct.jsx
+++ src/components/products/eachproduct.jsx
@@ -2,18 +2,27 @@
 'use client'
 import React from 'react';
 import { useAppSelector } from '@/lib/hooks';
-
+import {Button} from "@mui/material";
 const EachProduct = () => {
     // Initializing the redux to unleash the items to the new screen 
     const nextItem = useAppSelector((state) => state.product.selectedProduct);
-
     // Check if nextItem is not null before rendering its properties
-    return (
-        <div>
-            {nextItem && (
-    <></>
-            )}
-        </div>
+    return(
+    <div className='w-full h-96 flex  flex-row items-center justify-center mt-16 '>
+<div>
+    {/** the div will have to have the image of the  */}
+    <img src={nextItem.imageUrl} alt="The image for the product"/>
+</div>
+<div>
+    <div></div>
+    <Button sx={{backgroundColor:'orange',color:'white',textTransform:'inherit',height:'2rem',width:'12rem',
+"&:hover":{
+    backgroundColor:'black',
+    color:'white',
+}
+}}>Add To Cart</Button>
+</div>
+    </div>
     );
 };
 
