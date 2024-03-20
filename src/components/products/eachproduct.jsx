53d708705a83f399b20b8f3bd41f8ff05aaf1cc7
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-20 03:47:31 -1200

    I have the data in the cateories js and added quantity

--- src/components/products/eachproduct.jsx
+++ src/components/products/eachproduct.jsx
@@ -8,13 +8,18 @@ const EachProduct = () => {
     const nextItem = useAppSelector((state) => state.product.selectedProduct);
     // Check if nextItem is not null before rendering its properties
     return(
-    <div className='w-full  flex  flex-row items-center justify-center mt-24 '>
+    <div className='w-full  flex  flex-row items-center justify-center mt-24 gap-32 flex-wrap'>
 <div className='border-2 border-gray-200 rounded-lg p-4 w-56 h-56 shadow-sm  hover:text-white'>
     {/** the div will have to have the image of the  */}
-    <img src={nextItem.imageUrl} alt="The image for the product" className="w-52 h-48 rounded-sm flex items-center justify-center "/>
+    <img src={nextItem.imageUrl} alt="The image for the product" className="w-52 h-48 rounded-sm flex items-center justify-center self-center"/>
 </div>
 <div>
-    <div></div>
+    <div>
+        <h2>{nextItem.name}</h2>
+        <h2>This Cloth belongs to the {nextItem.group} group </h2>
+        <h4>${nextItem.price}</h4>
+        <h5>{nextItem.description}</h5>
+    </div>
     <Button sx={{backgroundColor:'orange',color:'white',textTransform:'inherit',height:'2rem',width:'12rem',"&:hover":{backgroundColor:'black',color:'white',}}}>Add To Cart</Button>
 </div>
     </div>
