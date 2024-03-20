5e049f1011d97b9b258d0c6d590d761b0d1baa6b
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-20 03:27:26 -1200

    now about to add the text for each product item

--- src/components/products/eachproduct.jsx
+++ src/components/products/eachproduct.jsx
@@ -8,19 +8,14 @@ const EachProduct = () => {
     const nextItem = useAppSelector((state) => state.product.selectedProduct);
     // Check if nextItem is not null before rendering its properties
     return(
-    <div className='w-full h-96 flex  flex-row items-center justify-center mt-16 '>
-<div>
+    <div className='w-full  flex  flex-row items-center justify-center mt-24 '>
+<div className='border-2 border-gray-200 rounded-lg p-4 w-56 h-56 shadow-sm  hover:text-white'>
     {/** the div will have to have the image of the  */}
-    <img src={nextItem.imageUrl} alt="The image for the product"/>
+    <img src={nextItem.imageUrl} alt="The image for the product" className="w-52 h-48 rounded-sm flex items-center justify-center "/>
 </div>
 <div>
     <div></div>
-    <Button sx={{backgroundColor:'orange',color:'white',textTransform:'inherit',height:'2rem',width:'12rem',
-"&:hover":{
-    backgroundColor:'black',
-    color:'white',
-}
-}}>Add To Cart</Button>
+    <Button sx={{backgroundColor:'orange',color:'white',textTransform:'inherit',height:'2rem',width:'12rem',"&:hover":{backgroundColor:'black',color:'white',}}}>Add To Cart</Button>
 </div>
     </div>
     );
