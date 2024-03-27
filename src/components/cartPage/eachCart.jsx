b6e5fd6bcc1d1b9c16f6d32658948c5b6d2b1d12
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-03-27 09:48:15 -1200

    Some other changes have been made

--- src/components/cartPage/eachCart.jsx
+++ src/components/cartPage/eachCart.jsx
@@ -1,10 +1,24 @@
+
+"use client"
 import React from 'react'
+import { useAppSelector } from '@/lib/hooks';
 
 
 const EachCartItem = () => {
+
+  //I want to access all the elements that is in the redux store 
+  const cartItems=useAppSelector((state)=>state.cart.cartItems);
+
   return (  
-    <div>
-      
+    //we have to first check if the place is empty then we display something else 
+    <div className='w-full flex flex-col items-center sm:gap-10 md:gap-24 lg:gap-32 gap-10 justify-center mt-24 h-full flex-wrap'>
+      <h2 className='flex items-center justify-center text-2xl font-sans font-semibold text-center'>Cart Items</h2>
+      {
+        cartItems.length===0?(
+<h6 className='text-center'>
+  Your Cart is Empty);
+</h6>
+):null}
     </div>
   );
 }
--- src/components/cartPage/main.jsx
+++ src/components/cartPage/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from '../homepage/navbar';
 import FooterComponent from '../homepage/footer';
+import EachCartItem from './eachCart';
 
 
 const MainCartPage = () => {
   return ( 
     <div>
 <Nav/>
+<EachCartItem/>
 <FooterComponent/>
     </div>
    );
